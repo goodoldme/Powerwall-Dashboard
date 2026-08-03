519b0bcc25fa9f60da6ecf63d188c57193bac5b7
Calvin Hobbes <5r91nrqo@duck.com>
2026-08-03 14:17:18 +0100

    Update InfluxDB health check method in setup.sh
    Change health check for InfluxDB setup from Docker inspect to HTTP ping.

--- setup.sh
+++ setup.sh
@@ -800,14 +800,10 @@ fi
 
 # Set up Influx
 echo "Waiting for InfluxDB to start..."
-until [ "$(docker inspect -f '{{if .State.Health}}{{.State.Health.Status}}{{end}}' influxdb 2>/dev/null || true)" = "healthy" ]; do
+until [ "$(curl -s -o /dev/null -w '%{http_code}' http://localhost:8086/ping)" = "204" ]; do
     printf '.'
     sleep 5
 done
-#until running http://localhost:8086/ping 204 2>/dev/null; do
-#    printf '.'
-#    sleep 5
-#done
 echo " up!"
 sleep 2
 echo "Setup InfluxDB Data... ('already exist' errors harmless)"
