1815f1dd3aee4622bf156fef534d27622d7b058d
Calvin Hobbes <5r91nrqo@duck.com>
2026-08-03 14:07:35 +0100

    Update InfluxDB health check in upgrade.sh
    Replace InfluxDB health check method with Docker inspect command.

--- upgrade.sh
+++ upgrade.sh
@@ -343,10 +343,14 @@ echo "Updating Powerwall-Dashboard stack..."
 # Update InfluxDB
 echo ""
 echo "Waiting for InfluxDB to start..."
-until running http://localhost:8086/ping 204 2>/dev/null; do
+until [ "$(docker inspect -f '{{if .State.Health}}{{.State.Health.Status}}{{end}}' influxdb 2>/dev/null || true)" = "healthy" ]; do
     printf '.'
     sleep 5
 done
+#until running http://localhost:8086/ping 204 2>/dev/null; do
+#    printf '.'
+#    sleep 5
+#done
 echo " up!"
 sleep 2
 echo ""
